e79a5af0a0ec6c0ce3442faa1477243d8a3b79dd
Eva <eva@byallmeans.work>
2024-08-31 20:46:48 +0300

    feat: add core components

---
+++ src/components/core/NavLink.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { Typography } from '@mui/material';
+import Link from 'next/link';
+import React, { FC } from 'react';
+import { usePathname } from 'next/navigation';
+import { grey, deepOrange } from '@mui/material/colors';
+
+export type Link = {
+  label: string;
+  href: string;
+};
+
+type NavLinkProps = {
+  link: Link;
+};
+
+export const NavLink: FC<NavLinkProps> = ({ link }) => {
+  const pathname = usePathname();
+
+  const isActive = pathname === link.href;
+
+  return (
+    <Link href={link.href}>
+      <Typography
+        component='span'
+        color={isActive ? deepOrange[700] : grey[900]}
+        fontWeight={600}
+        fontSize='1.8rem'
+      >
+        {link.label}
+      </Typography>
+    </Link>
+  );
+};
---
+++ src/components/core/ResponsiveImage.tsx
@@ -0,0 +1,40 @@
+import { Box } from '@mui/material';
+import Image from 'next/image';
+import React, { FC } from 'react';
+
+type Image = {
+  alt: string;
+  width: number;
+  height: number;
+  src: string;
+};
+
+type ResponsiveImageProps = {
+  img: Image;
+  borderRadius?: string | number;
+  aspectRatio?: number;
+};
+
+export const ResponsiveImage: FC<ResponsiveImageProps> = ({
+  img,
+  aspectRatio = 1, // default to 1:1 aspect ratio
+  borderRadius = 0,
+}) => {
+  return (
+    <Box
+      position='relative'
+      width='100%'
+      paddingTop={`${aspectRatio * 100}%`}
+      borderRadius={borderRadius}
+      overflow='hidden'
+    >
+      <Image
+        src={img.src}
+        fill
+        className='absolute inset-0'
+        role='img'
+        alt={img.alt}
+      />
+    </Box>
+  );
+};

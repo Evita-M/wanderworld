d1a43625903ed97e7cac87c24cc1f1d8668fafba
EvitaM <eva.matova6@gmail.com>
2025-03-30 19:20:13 +0300

    refactor: remove webpack alias configuration from next.config.mjs
    - Deleted the custom webpack alias configuration for '@' in next.config.mjs to simplify the setup and reduce complexity.

--- next.config.mjs
+++ next.config.mjs
@@ -9,13 +9,6 @@ const nextConfig = {
   images: {
     domains: ['fakeimg.pl'],
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(__dirname, 'src'),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;

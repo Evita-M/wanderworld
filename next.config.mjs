71c6d9af0952a9b45efb2aa80945a7bab7b4a4b0
EvitaM <eva.matova6@gmail.com>
2025-03-30 18:02:49 +0300

    refactor: update webpack configuration to simplify alias resolution
    - Modified webpack configuration in next.config.mjs to add a new alias '@' pointing to the 'src' directory for improved import clarity and consistency.

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,11 @@ const nextConfig = {
   images: {
     domains: ['fakeimg.pl'],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Ensure that all imports of 'yjs' resolve to the same instance
-      config.resolve.alias['yjs'] = path.resolve(__dirname, 'node_modules/yjs');
-    }
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, 'src'),
+    };
     return config;
   },
 };
